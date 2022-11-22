ec28a790dca059ede1417b7e444b91f159af03c3
SimoCrash <simone.crescenzi@hotmail.com>
2022-11-22 18:44:07 +0100

    Milestone 1 e Bonus

--- js/script.js
+++ js/script.js
@@ -1,25 +1,71 @@
 new Vue ({
     el: '#root',
     data: {
-        email: "",
+        // email: "",
         emails: [],
     },
     methods: {
-        getOneMail(){
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then(axiosResponse => {
-                this.email = axiosResponse.data.response;
-                this.emails.push(this.email);
-            })
-        },
+        // getOneMail(){
+        //     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        //     .then(axiosResponse => {
+        //         this.email.push(axiosResponse.data.response);
+        //     })
+        // },
         getTenMails(){
             for(let i = 0; i < 10; i++){
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-                .then(axiosResponse => {
-                    this.email = axiosResponse.data.response;
-                    this.emails.push(this.email);
+                .then((axiosResponse) => {
+                    // this.emails.push(this.email);
+                    this.emails.push(axiosResponse.data.response);
                 })
             }
         }
     }
 })
+
+/* UN PO DI PROVE
+
+SAREBBE COSI IL CODICE  axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(axiosResponse => {}
+
+
+function then(functionDaChiamareQuandoArrivaRisposta) {
+    //...
+    const oggettoDaPassare = {
+        data: {
+            response: ''
+        },
+
+    };
+    functionDaChiamareQuandoArrivaRisposta(oggettoDaPassare);
+}
+then(() => {
+
+});
+
+function get() {
+    return {
+        then() {
+
+        }
+    }
+}
+*/
+
+/*
+
+
+function a() {
+
+}
+a();
+
+const a = function() {
+
+};
+a();
+
+const a = () => {
+
+};
+a();
+*/
